2b52d563a6d1a74ce482024d0dd8d1f3906deb54
Muhammed raheem ali <M.Ali2924@student.leedsbeckett.ac.uk>
2023-10-19 21:18:22 +0100

    a

---
+++ week 1/task 4.py
@@ -0,0 +1,7 @@
+Matches_Played = 609
+Times_Batted = 1014
+Not_Out = 162
+Runs_Scored = 48426
+
+completed_Innings = (Times_Batted - Not_Out)
+print ("geffreys batting average was",  Runs_Scored/completed_Innings)
