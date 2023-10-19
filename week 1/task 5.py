b31f4165fa29db7d5310c8ec46136db6077cc010
Muhammed raheem ali <M.Ali2924@student.leedsbeckett.ac.uk>
2023-10-19 22:20:31 +0100

    a

---
+++ week 1/task 5.py
@@ -0,0 +1,2 @@
+number_of_students_left_over_113 = 113 % 24
+print (number_of_students_left_over_113)
---
+++ week 2/week 2 task 2.py
@@ -0,0 +1,4 @@
+Temp_Celsius = int(input(" enter a temparuture in celsius"))
+
+Temp_Farenheit = (Temp_Celsius * 1.8)+32
+print (Temp_Farenheit, "F is the same as",Temp_Celsius, "c" )
