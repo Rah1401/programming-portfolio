8ff5775b48bae7158f3fe3fa030169ae75b49fb3
Muhammed raheem ali <M.Ali2924@student.leedsbeckett.ac.uk>
2023-12-18 19:16:41 +0000

    a

--- week 5/script.py
+++ week 5/script.py
@@ -1,18 +1,1 @@
-import sys
 
-def report_os_platform():
-    """
-    Reports the operating system platform using the sys module.
-    """
-    platform = sys.platform
-    if platform.startswith('linux'):
-        print("Linux platform detected.")
-    elif platform.startswith('darwin'):
-        print("Mac OS platform detected.")
-    elif platform.startswith('win'):
-        print("Windows platform detected.")
-    else:
-        print("Unknown platform detected:", platform)
-
-if __name__ == "__main__":
-    report_os_platform()
